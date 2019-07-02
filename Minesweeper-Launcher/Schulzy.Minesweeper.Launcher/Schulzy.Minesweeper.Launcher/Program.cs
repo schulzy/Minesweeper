a9091118219b8462f9d876d6114ae06750ed9d22
schulzy@freemail.hu <schulzy@freemail.hu>
2019-07-02 22:53:42 +0200

    Modify solution/project structure

---
+++ Minesweeper-Launcher/Schulzy.Minesweeper.Launcher/Schulzy.Minesweeper.Launcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Schulzy.Minesweeper.Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
